696ad60441a0b5e20487085f4d07b2209c60857b
zy <1911660898@qq.com>
2019-09-06 21:22:35 +0800

    first commit

---
+++ src/life/bj/link/Node.java
@@ -0,0 +1,12 @@
+package life.bj.link;
+
+
+public class Node<T> {
+    public T data;
+    public Node next;
+
+    public Node(T data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+}
---
+++ src/life/bj/link/Palindromic.java
@@ -0,0 +1,70 @@
+package life.bj.link;
+
+/**
+ * 回文数
+ */
+public class Palindromic {
+
+    public static void main(String[] args) {
+//        Node<Character> node1 = new Node<>('a',null);
+//        Node<Character> node2 = new Node<>('b',node1);
+//        Node<Character> node3 = new Node<>('c',node2);
+        Node<Character> node4 = new Node<>('a',null);
+        Node<Character> head = new Node<>('a',node4);
+
+        boolean palindromic = isPalindromic(head);
+        System.out.println(palindromic);
+    }
+
+    /**
+     *  思路:
+     *   通过快慢指针找到中间节点，
+     *   然后反转中间节点后的所有节点
+     *   反转后的节点和前面的节点进行比较
+     *
+     *   时间复杂度为O(n)  空间复杂度为O(1)
+     *
+     *
+     * @param head
+     * @return
+     */
+    public static boolean isPalindromic(Node<?> head){
+        if(head == null){
+            return false;
+        }
+
+        Node<?> slow = head;
+        Node<?> fast = head;
+
+        Node<?> temp = null;
+
+        Node<?> compare = head;
+
+        // 找到中间节点
+        while(fast != null){
+            slow = slow.next;
+            fast = fast.next;
+            if (fast != null) {
+                fast = fast.next;
+            }
+        }
+        // 将满指针后的所有都逆序过来
+        while(slow != null){
+            Node<?> t = slow;
+            slow = slow.next;
+            t.next = temp;
+            temp = t;
+        }
+        // 进行比较
+        while(temp != null){
+            if(!temp.data.equals(compare.data)){
+                return false;
+            }
+            temp = temp.next;
+            compare = compare.next;
+
+        }
+        return true;
+    }
+
+}
